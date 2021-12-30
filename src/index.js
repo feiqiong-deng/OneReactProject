43735d46030e3ee532e4081e949f3d1b7f97fb91
feiqiong-deng <feiqiong.deng@outlook.com>
2021-12-29 19:37:18 -0500

    Add nested components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,45 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greeting() {
-  return (
-    <div>
-      <h1>This is Fei and this is my first component</h1>
-    </div>
-  );
+function Booklist() {
+  return <Book />;
 }
 
-// const Greeting = () => {
-//   return React.createElement('h1', {}, 'hello world');
+const Book = () => (
+  <article>
+    <Image />
+    <Title />
+    <Author />
+  </article>
+);
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/71WC3z4Wt0L._AC_UL640_QL65_.jpg"
+    alt=""
+  ></img>
+);
+
+const Title = () => <h1>The Book of Unusual Knowledge</h1>;
+
+const Author = () => <h4>by Publications International Ltd.</h4>;
+
+ReactDom.render(<Booklist />, document.getElementById("root"));
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <Person />
+//       <Message />
+//     </div>
+//     // <div>
+//     //   <h1>This is Fei and this is my first component</h1>
+//     // </div>
+//   );
 // }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+// const Person = () => <h2>Alice Tang</h2>;
+
+// const Message = () => {
+//   return <p>This is my message</p>;
+// };
