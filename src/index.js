773bfb2862310c29a8f4c080ea756750c026fec3
feiqiong-deng <feiqiong.deng@outlook.com>
2022-01-01 23:52:30 -0500

    Inline CSS and variables

--- src/index.js
+++ src/index.js
@@ -1,40 +1,35 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+//import CSS
+import "./index.css";
+
 function Booklist() {
-  return <Book />;
+  return (
+    <section className="booklist">
+      <Book />
+    </section>
+  );
 }
 
-const Book = () => (
-  <article>
-    <Image />
-    <Title />
-    <Author />
-    <Image />
-    <Title />
-    <Author />
-    <Image />
-    <Title />
-    <Author />
-    <Image />
-    <Title />
-    <Author />
-  </article>
-);
+const author = "by Publications International Ltd.";
 
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/71WC3z4Wt0L._AC_UL640_QL65_.jpg"
-    alt=""
-  ></img>
-);
+const Book = () => {
+  const title = "I love you to the Moon and Back";
 
-const Title = () => <h1>The Book of Unusual Knowledge</h1>;
-
-const Author = () => <h4>by Publications International Ltd.</h4>;
-
+  return (
+    <article className="book">
+      <img
+        src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL640_FMwebp_QL65_.jpg"
+        alt=""
+      ></img>
+      <h1>{title.toUpperCase()}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
 ReactDom.render(<Booklist />, document.getElementById("root"));
-
+// ***********************************************************************
 // function Greeting() {
 //   return (
 //     <div>
@@ -52,3 +47,29 @@ ReactDom.render(<Booklist />, document.getElementById("root"));
 // const Message = () => {
 //   return <p>This is my message</p>;
 // };
+// ***********************************************************************
+
+// ***********************************************************************
+// const Book = () => (
+//   <article className="book">
+//     <Image />
+//     <Title />
+//     <Author />
+//   </article>
+// );
+
+// const Image = () => (
+//   <img
+//     src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL640_FMwebp_QL65_.jpg"
+//     alt=""
+//   ></img>
+// );
+
+// const Title = () => <h1>The Book of Unusual Knowledge</h1>;
+
+// const Author = () => (
+//   <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+//     by Publications International Ltd.
+//   </h4>
+// );
+// ***********************************************************************
