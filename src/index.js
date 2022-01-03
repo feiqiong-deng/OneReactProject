88db2978ea459e0cbb139ff9f51ba7ba8d7b1d47
feiqiong-deng <feiqiong.deng@outlook.com>
2022-01-03 10:51:46 -0500

    Props

--- src/index.js
+++ src/index.js
@@ -4,27 +4,47 @@ import ReactDom from "react-dom";
 //import CSS
 import "./index.css";
 
+// set up vars
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL640_FMwebp_QL65_.jpg",
+  title: "I love you to the Moon and Back",
+  author: "by Publications International Ltd.",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL640_FMwebp_QL65_.jpg",
+  title: "Our Class is a Family",
+  author: "by Shannon Olsen and Sandie Sonke",
+};
+
+// const author = "by Publications International Ltd.";
+// const title = "I love you to the Moon and Back";
+// const img =
+//   "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL640_FMwebp_QL65_.jpg";
+
 function Booklist() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const author = "by Publications International Ltd.";
-
-const Book = () => {
-  const title = "I love you to the Moon and Back";
-
+const Book = (props) => {
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL640_FMwebp_QL65_.jpg"
-        alt=""
-      ></img>
-      <h1>{title.toUpperCase()}</h1>
-      <h4>{author}</h4>
+      <img src={props.img} alt=""></img>
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
