509ff956058b192552d83e0b01dd0996a7846155
feiqiong-deng <feiqiong.deng@outlook.com>
2022-01-03 23:35:28 -0500

    modify the list

--- src/index.js
+++ src/index.js
@@ -20,17 +20,21 @@ import "./index.css";
 // set up a proper list
 const books = [
   {
+    id: 1,
     img: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL640_FMwebp_QL65_.jpg",
     title: "I love you to the Moon and Back",
     author: "by Publications International Ltd.",
   },
 
   {
+    id: 2,
     img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL640_FMwebp_QL65_.jpg",
     title: "Our Class is a Family",
     author: "by Shannon Olsen and Sandie Sonke",
   },
+
   {
+    id: 3,
     img: "https://m.media-amazon.com/images/I/91HHxxtA1wL._AC_UL640_FMwebp_QL65_.jpg",
     title: "The Wonderful Things You Will Be",
     author: "by Emily Winfield Martin",
@@ -46,7 +50,7 @@ function Booklist() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book book={book}></Book>;
+        return <Book key={book.id} book={book}></Book>;
       })}
       {/* <Book
         img={firstBook.img}
